c3f262911ededc5ef9ab65c8fa5ab14badb4e0e5
joychae <sooie35@gmail.com>
2021-03-02 18:43:54 +0900

    3rd_HTML Setting

--- prac_scraping.py
+++ prac_scraping.py
@@ -1,25 +1,28 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import time
-from selenium.common.exceptions import NoSuchElementException
-from pymongo import MongoClient
-import requests
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome("./chromedriver")  # 드라이버를 실행합니다.
 
 
-client = MongoClient("내AWS아이피", 27017, username="아이디", password="비밀번호")
-db = client.dbsparta_plus_week3
+url = "https://www.melon.com/chart/day/index.htm"
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data = requests.get(url, headers=headers)
 
-driver = webdriver.Chrome("./chromedriver")
+driver.get(url)  # 드라이버에 해당 url의 웹페이지를 띄웁니다.
+sleep(5)  # 페이지가 로딩되는 동안 5초 간 기다립니다.
 
-url = "http://matstar.sbs.co.kr/location.html"
+req = driver.page_source  # html 정보를 가져옵니다.
+driver.quit()  # 정보를 가져왔으므로 드라이버는 꺼줍니다.
 
-driver.get(url)
-time.sleep(5)
+# soup = BeautifulSoup(data.text, 'html.parser')
+soup = BeautifulSoup(req, "html.parser")  # 가져온 정보를 beautifulsoup으로 파싱해줍니다.
 
-req = driver.page_source
-driver.quit()
+songs = soup.select("#frm > div > table > tbody > tr")
+print(len(songs))
 
-soup = BeautifulSoup(req, "html.parser")
-
-places = soup.select("ul.restaurant_list > div > div > li > div > a")
-print(len(places))
+for song in songs:
+    title = song.select_one("td > div > div.wrap_song_info > div.rank01 > span > a").text
+    artist = song.select_one("td > div > div.wrap_song_info > div.rank02 > span > a").text
+    likes = song.select_one("td > div > button.like > span.cnt").text
+    print(title, artist, likes)
